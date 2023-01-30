b6471bd6d7103fbb1989365198f40cb18dd1457a
JUHYUN <JUHYUN@DESKTOP-1ES41BH>
2023-01-30 22:30:08 +0900

    db pw change

--- BBS/src/main/java/bbs/BbsDAO.java
+++ BBS/src/main/java/bbs/BbsDAO.java
@@ -15,7 +15,7 @@ public class BbsDAO {
 		try {
 			String dbURL = "jdbc:mysql://sts-jsp-bbs.cdgyxpkeva3i.ap-northeast-2.rds.amazonaws.com:3306/BBS";
 			String dbID = "root";
-			String dbPassword = "root";
+			String dbPassword = "roottoorr";
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
 		} catch (Exception e) {
